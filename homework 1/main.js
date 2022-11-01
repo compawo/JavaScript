b579299f33ec517e0270a54db1dcf8be6c07e763
compawo <pundorkamila@gmail.com>
2022-11-01 19:00:21 +0100

    task1

---
+++ homework 1/main.js
@@ -0,0 +1,18 @@
+"use strict";
+let b = `hello`;
+let b1 = `owu`;
+let b2 = `com`;
+let b3 = `ua`;
+let b4 = 1;
+let b5 = 10;
+let b6 = -999;
+let b7 = 123;
+let b8 = 3.14;
+let b9 = 2.7;
+let b10 = 16;
+let b11 = 2 + 2 === 4;
+let b12 = 2 / 2 === 4;
+
+
+console.log(b + b1 + b2 + b3 + b4 + b5 + b6 + b7 + b8 + b9 + b10 + b11 + b12)
+console.log( `${b} ${b1} ${b2} ${b3} ${b4} ${b5} ${b6} ${b7} ${b8} ${b9} ${b10} ${b11} ${b12}`)
