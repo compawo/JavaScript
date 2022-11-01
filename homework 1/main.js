8f468ea11fa763ed95b22cc9588bd87e241ee3da
compawo <pundorkamila@gmail.com>
2022-11-01 19:53:08 +0100

    finishTask

--- homework 1/main.js
+++ homework 1/main.js
@@ -1,18 +1,43 @@
 "use strict";
-let b = `hello`;
-let b1 = `owu`;
+let s = `hello`;
+console.log(s)
+const b1 = `owu`;
+console.log(b1)
 let b2 = `com`;
+console.log(b2)
 let b3 = `ua`;
-let b4 = 1;
-let b5 = 10;
-let b6 = -999;
-let b7 = 123;
-let b8 = 3.14;
+console.log(b3)
+let num = 10;
+console.log(num)
+let num1 = -999;
+console.log(num1)
+let num2 = 123;
+console.log(num2)
+let num3 = 1;
+console.log(num3)
+const PI = 3.14;
+console.log(PI)
 let b9 = 2.7;
+console.log(b9)
 let b10 = 16;
-let b11 = 2 + 2 === 4;
-let b12 = 2 / 2 === 4;
+console.log(b10)
+let resolve = 2 + 2 === 4;
+console.log(resolve)
+let resolve1 = 2 / 2 === 4;
+console.log(resolve1)
+
+let firstName = `Kamila`;
+let middleName = `Olehivna`;
+let lastName = `Pundor`;
+let person = `${firstName} ${middleName} ${lastName} `;
+console.log(person)
 
 
-console.log(b + b1 + b2 + b3 + b4 + b5 + b6 + b7 + b8 + b9 + b10 + b11 + b12)
-console.log( `${b} ${b1} ${b2} ${b3} ${b4} ${b5} ${b6} ${b7} ${b8} ${b9} ${b10} ${b11} ${b12}`)
+let a = 100;
+console.log(typeof a)
+let b = '100';
+console.log(typeof b)
+let c = true;
+console.log(typeof c)
+
+
