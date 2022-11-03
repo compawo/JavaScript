ca3e10928a33d3398bd390a2799dc0274a4b766f
compawo <pundorkamila@gmail.com>
2022-11-03 15:28:37 +0100

    finishHomeWork

---
+++ homework 2/main.js
@@ -0,0 +1,100 @@
+let arr = [`hello`, `bye`, `world`, `year`, `log`, false, true, `like`, `be`, [10, 55, 190.]];
+console.log(arr[0])
+console.log(arr[1])
+console.log(arr[2])
+console.log(arr[3])
+console.log(arr[4])
+console.log(arr[5])
+console.log(arr[6])
+console.log(arr[7])
+console.log(arr[8])
+console.log(arr[9])
+
+let book = {
+    title: `The Little Prince`,
+    pageCount: 96,
+    genre: `novel`,
+}
+
+console.log(book)
+let book2 = {
+    title: `Prince`,
+    pageCount: 960,
+    genre: `romance`,
+}
+
+console.log(book2)
+
+let book3 = {
+    title: `The Little `,
+    pageCount: 496,
+    genre: `drama`,
+}
+
+console.log(book3)
+
+let list1 = {
+    title: `The Little Prince`,
+    pageCount: 96,
+    genre: `novel`,
+    authors: [{
+        name: `Antoine de Saint-Exupery`,
+        age: 44,
+    }]
+}
+
+console.log(list1)
+
+let list2 = {
+    title: `The Little =`,
+    pageCount: 796,
+    genre: `novel`,
+    authors: [{
+        name: `Antoine `,
+        age: 40,
+    }]
+}
+
+console.log(list2)
+
+let list3 = {
+    title: `The Prince`,
+    pageCount: 966,
+    genre: `novel`,
+    authors: [{
+        name: `Saint-Exupery`,
+        age: 21,
+    }]
+}
+
+console.log(list3)
+
+let user = [
+    {name: `Loui`, username: `lfgbnj`, password: 12345678,},
+    {name: `Tom`, username: `lscvh`, password: 12345678,},
+    {name: `Ann`, username: `lhg`, password: 66435678,},
+    {name: `Don`, username: `greo`, password: 3443678,},
+    {name: `Ron`, username: `errgfz`, password: 3434678,},
+    {name: `Leon`, username: `lthrs`, password: 1234543,},
+    {name: `Katy`, username: `zsthro`, password: 1243678,},
+    {name: `Bob`, username: `lthzsdro`, password: 14343348,},
+    {name: `Dian`, username: `dyxjo`, password: 1545478,},
+    {name: `Fan`, username: `dtjjdtjt`, password: 15544678,}
+]
+
+
+
+console.log(user[0].password)
+console.log(user[1].password)
+console.log(user[2].password)
+console.log(user[3].password)
+console.log(user[4].password)
+console.log(user[5].password)
+console.log(user[6].password)
+console.log(user[7].password)
+console.log(user[8].password)
+console.log(user[9].password)
+
+
+
+
