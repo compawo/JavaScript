ba7b46e06c0a8bbda28a1ae94b30a248feda9e00
compawo <pundorkamila@gmail.com>
2022-11-10 16:18:00 +0100

    task1

--- homework 4/main.js
+++ homework 4/main.js
@@ -24,7 +24,7 @@ for (let us of form) {
 
 //- За допомогою циклу for і document.write() вивести 10 блоків div c довільним текстом і індексом всередині
 
-let tex = [
+let text = [
     {name: 'vasya', age: 31, status: false},
     {name: 'petya', age: 30, status: true},
     {name: 'kolya', age: 29, status: true},
@@ -37,10 +37,11 @@ let tex = [
     {name: 'olya', age: 31, status: false}
 ]
 
-for (let i=0; i < tex.length; i++) {
+for (let i = 0; i < text.length; i++) {
+
     document.write(
         ` 
-  <div>Object ${tex[i]}</div>
+  <div>Object ${i + 1}</div>
  
         `
     )
@@ -48,7 +49,81 @@ for (let i=0; i < tex.length; i++) {
 
 //- За допомогою циклу while вивести в документ 20 блоків h1 c довільним текстом всередині.
 
+let files = [
+    {name: 'vasya', age: 31, status: false},
+    {name: 'petya', age: 30, status: true},
+    {name: 'kolya', age: 29, status: true},
+    {name: 'olya', age: 28, status: false},
+    {name: 'max', age: 30, status: true},
+    {name: 'anya', age: 31, status: false},
+    {name: 'oleg', age: 28, status: false},
+    {name: 'andrey', age: 29, status: true},
+    {name: 'masha', age: 30, status: true},
+    {name: 'olya', age: 31, status: false},
+    {name: 'vasya', age: 31, status: false},
+    {name: 'petya', age: 30, status: true},
+    {name: 'kolya', age: 29, status: true},
+    {name: 'olya', age: 28, status: false},
+    {name: 'max', age: 30, status: true},
+    {name: 'anya', age: 31, status: false},
+    {name: 'oleg', age: 28, status: false},
+    {name: 'andrey', age: 29, status: true},
+    {name: 'masha', age: 30, status: true},
+    {name: 'olya', age: 31, status: false},
+]
+let i = 0;
+while (i < files.length) {
+    let user = files[i]
+    if (user.status) {
+        document.write(
+        `
+        <div>
+        <h1>Text</h1>
+        </div>
+        `
+        )
+    }
+    i++
+}
+
 //- За допомогою циклу while вивести в документ 20 блоків h1 c довільним текстом і індексом всередині.
+
+let file = [
+    {name: 'vasya', age: 31, status: false},
+    {name: 'petya', age: 30, status: true},
+    {name: 'kolya', age: 29, status: true},
+    {name: 'olya', age: 28, status: false},
+    {name: 'max', age: 30, status: true},
+    {name: 'anya', age: 31, status: false},
+    {name: 'oleg', age: 28, status: false},
+    {name: 'andrey', age: 29, status: true},
+    {name: 'masha', age: 30, status: true},
+    {name: 'olya', age: 31, status: false},
+    {name: 'vasya', age: 31, status: false},
+    {name: 'petya', age: 30, status: true},
+    {name: 'kolya', age: 29, status: true},
+    {name: 'olya', age: 28, status: false},
+    {name: 'max', age: 30, status: true},
+    {name: 'anya', age: 31, status: false},
+    {name: 'oleg', age: 28, status: false},
+    {name: 'andrey', age: 29, status: true},
+    {name: 'masha', age: 30, status: true},
+    {name: 'olya', age: 31, status: false},
+]
+let ind = 0;
+while (ind < file.length) {
+    let user = file[ind]
+    if (user.status) {
+        document.write(
+            `
+        <div>
+        <h1>Text</h1>
+        </div>
+        `
+        )
+    }
+    i++
+}
 
 //- Використовуючи данні з масиву, за допомоги document.write та циклу
 // побудувати структуру по шаблону Масив
@@ -72,8 +147,8 @@ let users = [
     {name: 'max', age: 31, status: true}
 ];
 
-for( let inf of users) {
-    if (inf.status){
+for (let inf of users) {
+    if (inf.status) {
         document.write(
             `
             <div>
@@ -84,8 +159,8 @@ for( let inf of users) {
     }
 
 }
-for( let inf of users) {
-    if (!inf.status){
+for (let inf of users) {
+    if (!inf.status) {
         document.write(
             `
             <div>
@@ -96,15 +171,17 @@ for( let inf of users) {
     }
 }
 
-for( let inf of users) {
-    if (inf.age){
+for (let i = 0; i < users.length; i++) {
+    const user = users[i];
+    if (user.age >= 30) {
         document.write(
             `
             <div>
-            <h1>${inf.name}</h1>
-            <p>${inf.age>30}</p>
+            <h1>${user.name}</h1>
+            <p>${user.age}</p>
             </div> 
             `
         )
     }
+
 }
